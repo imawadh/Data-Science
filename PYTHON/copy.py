b64ecc67fc94a2a3ecf1bf76931a544fe296f9ed
Awadh Kishor Singh <148574291+imawadh@users.noreply.github.com>
2025-01-31 15:41:50 +0530

    Post Pruning in Decision Tree

--- PYTHON/copy.py
+++ PYTHON/copy.py
@@ -9,3 +9,12 @@ print(original)  # Output: [[99, 2], [3, 4]] (shallow copy reflects changes)
 
 deep[0][0] = 42
 print(original)  # Output: [[99, 2], [3, 4]] (deep copy does not reflect changes)
+
+
+
+
+# a =  10
+# b = a
+# a = 56
+# print("a = ",a)
+# print("b = ",b) --- > Here in primitive data type all the copies are deep only
